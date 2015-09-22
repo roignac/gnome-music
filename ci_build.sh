bde91c3b0b4d1bb8af345394fecc7897d2c25230
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-09-22 13:31:43 +0200

    Make a SRPM package for nightly repo

--- ci_build.sh
+++ ci_build.sh
@@ -16,7 +16,20 @@ git submodule update --init
 RELEASE=$(git describe --exact-match HEAD)
 
 if [ -z "$RELEASE" ]; then
-    make
+    make distcheck
+
+    export VERSION=$(git describe --abbrev=0 --tags)
+    export RELEASE="$(date +"%Y%m%d")git$(git rev-parse --short=8 HEAD)"
+    echo "Making a fedora copr build"
+    mkdir -p /root/rpmbuild/SOURCES
+    cp gnome-music-*.tar.xz /root/rpmbuild/SOURCES/gnome-music-$VERSION.tar.xz
+    cd /root
+    git clone git://pkgs.fedoraproject.org/gnome-music.git
+    cd gnome-music
+    sed -i "s,Version:.*,Version: $VERSION," gnome-music.spec
+    sed -i "s,Release:.*,Release: $RELEASE%{?dist}," gnome-music.spec
+    rpmbuild -bs gnome-music.spec
+    ls -la /root/rpmbuild/SRPMS/*.src.rpm
 else
     echo "New release: $RELEASE"
     make distcheck
