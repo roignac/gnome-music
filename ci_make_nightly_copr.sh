d1818e617846d4dd0a49173e68ab277358e74269
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-09-22 14:33:58 +0200

    travisci: show VERSION and RELEASE in debug output

--- ci_make_nightly_copr.sh
+++ ci_make_nightly_copr.sh
@@ -2,6 +2,8 @@
 export VERSION=$(git describe --abbrev=0 --tags)
 export RELEASE="$(date +"%Y%m%d")git$(git rev-parse --short=8 HEAD)"
 echo "Making a fedora copr build"
+echo "VERSION=$VERSION"
+echo "VERSION=$RELEASE"
 mkdir -p /root/rpmbuild/SOURCES
 cp gnome-music-*.tar.xz /root/rpmbuild/SOURCES/gnome-music-$VERSION.tar.xz
 cd /root
