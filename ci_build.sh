d68cba8067078103915631e42a51379f1ebad760
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-09-22 13:56:16 +0200

    travisci: rework ci scripts

--- ci_build.sh
+++ ci_build.sh
@@ -13,13 +13,21 @@ dnf install -q -y gnome-common make which intltool git xz rpm-build
 git submodule update --init
 ./autogen.sh
 
-RELEASE=$(git describe --exact-match HEAD)
+if [ "$PR" -ne "false" ]; then
+    make
+else
+    echo "Not a PR, making a new nightly release in COPR"
+    make distcheck
 
-if [ -z "$RELEASE" ]; then
-    if [ "$PR" -e "false" ]; then
-        echo "Not a PR, making a new nightly release in COPR"
+    RELEASE=$(git describe --exact-match HEAD)
+    if [ -z "$RELEASE" ]; then
+        echo "Uploading new release $RELEASE to master.gnome.org"
         make distcheck
-
+        FILENAME=gnome-music-$RELEASE.tar.xz
+        echo "Built tarball $FILENAME"
+        # TODO: copy tarball to master.gnome.org and run ftp-install there
+    else
+        echo "Building a nightly SRPM and uploading to copr"
         export VERSION=$(git describe --abbrev=0 --tags)
         export RELEASE="$(date +"%Y%m%d")git$(git rev-parse --short=8 HEAD)"
         echo "Making a fedora copr build"
@@ -33,16 +41,5 @@ if [ -z "$RELEASE" ]; then
         rpmbuild -bs gnome-music.spec
         ls -la /root/rpmbuild/SRPMS/*.src.rpm
         # TODO: copy srpm to a public space and start copr build
-    else:
-        make
-    fi
-
-else
-    if [ "$PR" -e "false" ]; then
-        echo "New release: $RELEASE"
-        make distcheck
-        FILENAME=gnome-music-$RELEASE.tar.xz
-        echo "Built tarball $FILENAME"
-        # TODO: copy tarball to master.gnome.org and run ftp-install there
     fi
 fi
