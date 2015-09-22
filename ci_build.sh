f7b789d6df9fed4a654bbf68099a2c5129ebbb81
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-09-22 14:02:36 +0200

    travisci: move nightly copr creation to a separate file and ignore its exit code

--- ci_build.sh
+++ ci_build.sh
@@ -28,18 +28,6 @@ else
         # TODO: copy tarball to master.gnome.org and run ftp-install there
     else
         echo "Building a nightly SRPM and uploading to copr"
-        export VERSION=$(git describe --abbrev=0 --tags)
-        export RELEASE="$(date +"%Y%m%d")git$(git rev-parse --short=8 HEAD)"
-        echo "Making a fedora copr build"
-        mkdir -p /root/rpmbuild/SOURCES
-        cp gnome-music-*.tar.xz /root/rpmbuild/SOURCES/gnome-music-$VERSION.tar.xz
-        cd /root
-        git clone git://pkgs.fedoraproject.org/gnome-music.git
-        cd gnome-music
-        sed -i "s,Version:.*,Version: $VERSION," gnome-music.spec
-        sed -i "s,Release:.*,Release: $RELEASE%{?dist}," gnome-music.spec
-        rpmbuild -bs gnome-music.spec
-        ls -la /root/rpmbuild/SRPMS/*.src.rpm
-        # TODO: copy srpm to a public space and start copr build
+        sh ci_make_nightly_copr.sh || true  # don't fail the build if nightly cannot be submitted
     fi
 fi
---
+++ ci_make_nightly_copr.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+export VERSION=$(git describe --abbrev=0 --tags)
+export RELEASE="$(date +"%Y%m%d")git$(git rev-parse --short=8 HEAD)"
+echo "Making a fedora copr build"
+mkdir -p /root/rpmbuild/SOURCES
+cp gnome-music-*.tar.xz /root/rpmbuild/SOURCES/gnome-music-$VERSION.tar.xz
+cd /root
+git clone git://pkgs.fedoraproject.org/gnome-music.git
+cd gnome-music
+sed -i "s,Version:.*,Version: $VERSION," gnome-music.spec
+sed -i "s,Release:.*,Release: $RELEASE%{?dist}," gnome-music.spec
+rpmbuild -bs gnome-music.spec
+ls -la /root/rpmbuild/SRPMS/*.src.rpm
+# TODO: copy srpm to a public space and start copr build
